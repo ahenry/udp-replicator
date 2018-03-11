696565667eb45390403ab0100c6b84ce2b8b4a5f
Austin Henry <ahenry-udp@twocanoe.ca>
2018-03-10 21:49:07 -0800

    First version that does something configurable - there's a config hashmap, stuff gets looked up - multiple packets can be sent (packet replicator functionality) for   a given input packet

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,78 @@ extern crate pnet;
 
 use pnet::packet::{MutablePacket, Packet};
 use pnet::packet::udp::{ipv4_checksum, UdpPacket, MutableUdpPacket};
-use pnet::packet::ipv4::{checksum, MutableIpv4Packet};
+use pnet::packet::ipv4;
+use pnet::packet::ipv4::{Ipv4Packet, MutableIpv4Packet};
+use pnet::packet::ipv6;
+use pnet::packet::ipv6::{Ipv6Packet, MutableIpv6Packet};
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::transport::{transport_channel, udp_packet_iter, ipv4_packet_iter};
 use pnet::transport::TransportProtocol::Ipv4;
 use pnet::transport::TransportChannelType::{Layer3, Layer4};
 
+use std::collections::HashMap;
 use std::iter::repeat;
+use std::net::{Ipv4Addr, SocketAddrV4};
 use std::str;
 
+fn v4_to_v4<'a>(packet: &'a [u8], destination: &SocketAddrV4, source: Option<&Ipv4Addr>) -> Option<MutableIpv4Packet<'a>> {
+
+    let mut new_packet = MutableIpv4Packet::owned(packet.to_vec())?;
+
+    if let Some(src_addr) = source {
+        new_packet.set_source(*src_addr);
+    }
+    let new_source = new_packet.get_source();
+    let new_dest = *destination.ip();
+    new_packet.set_destination(new_dest);
+
+    {
+        let mut new_udp_packet = MutableUdpPacket::new(new_packet.payload_mut())?;
+        new_udp_packet.set_destination(destination.port());
+        let checksum = ipv4_checksum(&new_udp_packet.to_immutable(), &new_source, &new_dest);
+        new_udp_packet.set_checksum(checksum);
+    }
+    
+    let new_checksum = ipv4::checksum(&new_packet.to_immutable());
+    new_packet.set_checksum(new_checksum);
+    
+    Some(new_packet)
+}
+
+type DestMap = HashMap<u16, Vec<SocketAddrV4>>;
+enum IpPacket<'p> {
+    V4(&'p Ipv4Packet<'p>),
+    V6(&'p Ipv6Packet<'p>),
+}
+
+// TODO: this needs to return multiple addresses, etc
+fn get_destinations<'a>(packet: &IpPacket, dest_map: &'a DestMap) -> Option<&'a Vec<SocketAddrV4>> {
+    let dest = match *packet {
+        IpPacket::V4(ref p) => {
+            let udp_packet = UdpPacket::new(p.payload())?;
+            udp_packet.get_destination()
+        }
+        IpPacket::V6(ref _p) =>  unimplemented!("haven't done v6 support yet")
+    };
+
+    dest_map.get(&dest)
+}
+
 fn main() {
-    // udp only for the moment
-    //let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Udp));
+
+    // pretend we have parsed a config file, and generated this structure
+    //  mapping local ports to the destination address
+    //  TODO:
+    //  * the destination needs to expand to handle load-balance and replicate
+    //    workloads
+    //  * the source may need to expand to be able to consider the local address
+    //    as well as the port, but it's simpler for now just to use the port
+    let dest_map: DestMap = [
+        (333, vec!["127.1.0.1:3333".parse().unwrap(), "127.1.100.88:3333".parse().unwrap()]),
+        (33, vec!["127.1.1.1:444".parse().unwrap()]),
+    ].iter().cloned().collect();
+
+    // TODO: probably need to replicate this whole thing for ipv6 too
     let protocol = Layer3(IpNextHeaderProtocols::Udp);
 
     let (mut tx, mut rx) = match transport_channel(4096, protocol) {
@@ -28,46 +88,21 @@ fn main() {
 
     let mut iter = ipv4_packet_iter(&mut rx);
     loop {
-        if let Ok((packet, addr)) = iter.next() {
-            //println!("{:?}, {:?}", packet, addr);
-            if packet.get_destination() == "127.0.0.3".parse::<std::net::Ipv4Addr>().unwrap() {
+        if let Ok((packet, _addr)) = iter.next() {
+//            println!("{:?}, {:?}", packet, _addr);
+            
+            let destinations = get_destinations(&IpPacket::V4(&packet), &dest_map);
+//            println!("{:?}", destinations);
+            // if we didn't find a destination, we don't handle this packet
+            if let None = destinations {
                 continue;
             }
 
-            if let Some(udp_packet) = UdpPacket::new(packet.payload()) {
-                //println!("{:?}", udp_packet);
-                if udp_packet.get_destination() == 333 {
-                    println!("{:?}", str::from_utf8(udp_packet.payload()).unwrap());
-                } else {
-                    continue;
-                }
+            let destinations = destinations.unwrap();
 
-                let old_source = packet.get_source();
-                let old_dest = packet.get_destination();
-                let new_dest: std::net::Ipv4Addr = "127.0.0.3".parse().unwrap();
-
-                let mut backing: Vec<u8> = repeat(0u8).take(packet.packet().len()).collect();
-                let mut new_packet = MutableIpv4Packet::new(&mut backing).unwrap();
-                new_packet.clone_from(&packet);
-                new_packet.set_destination(new_dest);
-
-                {
-                    let mut new_udp_packet = MutableUdpPacket::new(new_packet.payload_mut())
-                        .unwrap();
-                    let checksum =
-                        ipv4_checksum(&new_udp_packet.to_immutable(), old_source, new_dest);
-                    new_udp_packet.set_checksum(checksum);
-                    //println!("{:?}", new_udp_packet);
-                }
-
-                let new_checksum = checksum(&new_packet.to_immutable());
-                new_packet.set_checksum(new_checksum);
-
-                //println!("{:?}", new_packet);
-                let _ = tx.send_to(new_packet, "127.0.0.3".parse().unwrap());
-
-            } else {
-                continue;
+            for dest in destinations {
+                let new_packet = v4_to_v4(packet.packet(), dest, None).unwrap();
+                tx.send_to(new_packet, std::net::IpAddr::V4(*dest.ip()));
             }
         }
     }
